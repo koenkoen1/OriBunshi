0725a4e490503fc71379f2f99fc8724e790884f7
lameturkey <koenk1@gmail.com>
2018-11-05 12:41:07 +0100

    minor changes

--- code/molecule.py
+++ code/molecule.py
@@ -1,9 +1,13 @@
 from amino_acid import Amino_Acid
 
 class Molecule(object):
+
+    # init function
     def __init__(self, sequence):
         self.sequence = []
         coordinates = x, y = (0, 0)
+
+        # produces a list of amino acids with coordinates (0, 0), (0, 1) etc
         for letter in sequence:
             self.sequence.append(Amino_Acid(letter, coordinates))
             y = y + 1
