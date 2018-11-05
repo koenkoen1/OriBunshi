624356c22e063359e584303085b420bf54fdce17
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-11-05 13:11:44 +0100

    main.py basic implementation

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# Placeholder
+import os, sys
+directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(directory, "code"))
+sys.path.append(os.path.join(directory, "code", "objects"))
+
+from molecule import Molecule
+
+def main():
+    # gib fir lin
+    with open('data/input.txt', 'r') as input:
+        line = input.readline()
+        mol = Molecule(line)
+        print(mol)
+
+if __name__ == "__main__":
+    main()
