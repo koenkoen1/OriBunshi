4feafc7d6f24f6ce275841487d65a18fb963c976
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-11-07 11:50:50 +0100

    implementation for user input in main, not yet looped

--- main.py
+++ main.py
@@ -5,16 +5,32 @@ sys.path.append(os.path.join(directory, "code", "objects"))
 
 from molecule import Molecule
 
+directions = ["left", "right"]
+
 def main():
     # gib fir lin
-    with open('data/input.txt', 'r') as input:
-        line = input.readline()
-        mol = Molecule(line)
-        print(mol.stability())
-        print(mol)
-        mol.turn(2, 'Left')
-        mol.turn(3, 'Left')
-        print(mol)
-        print(mol.stability())
+    with open('data/input.txt', 'r') as f:
+        line = f.readline()
+        print(f"current sequence is {line}")
+        molecule = Molecule(line)
+        command = input("command: ")
+
+        try:
+            command = command.split()
+        except:
+            print("use: turn id direction")
+
+        if (command[0] == "turn" and len(command) == 3 and
+            command[2] in directions):
+            try:
+                id = int(command[1])
+                molecule.turn(id, command[2])
+                print(molecule.stability())
+            except ValueError:
+                print("id was not a number")
+        else:
+            print("use: turn id direction")
+
+
 if __name__ == "__main__":
     main()
