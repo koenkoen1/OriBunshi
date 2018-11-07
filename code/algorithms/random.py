111f30c85b1273483d83d85f304a342bd048b471
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-11-07 10:29:43 +0100

    implemented path for random.py

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -1,5 +1,12 @@
+import os, sys
+directory = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(directory)
+sys.path.append(os.path.join(parentdir, "objects"))
 
 
-from ..objects import Molecule
+from molecule import Molecule
 
 molecule = Molecule("AAAAAAAAAAAAAAAAAAAAAAAAAa")
+
+
+print(molecule)
