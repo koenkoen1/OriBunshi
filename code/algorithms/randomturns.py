b7c5309a2d730a7daff2c4f3ab4834aa9668d37b
lameturkey <koenk1@gmail.com>
2018-11-09 17:40:54 +0100

    fixed the stability method

--- code/algorithms/randomturns.py
+++ code/algorithms/randomturns.py
@@ -10,10 +10,12 @@ import random
 import copy
 def randomturns(molecule, x):
     i = 0
-    lowest = 1
+    lowest = molecule.stability()
+    lowestsequence = copy.deepcopy(molecule.sequence)
+    print(lowest)
     while i < x:
         print(i)
-        backupmolecule = copy.deepcopy(molecule)
+        backupseqence = copy.deepcopy(molecule.sequence)
         randomnode = random.randint(0, len(molecule.sequence) - 1)
         randomdirection = random.randint(0, 1)
         if randomdirection == 0:
@@ -21,10 +23,11 @@ def randomturns(molecule, x):
         else:
             randomdirection = 'Left'
         molecule.turn(randomnode, randomdirection)
-        if not molecule.check_vadility():
-            molecule = copy.deepcopy(backupmolecule)
+        if  not molecule.check_vadility():
+            molecule.sequence = backupseqence
         elif lowest > molecule.stability():
+            print("replacing")
             lowest = molecule.stability()
-            lowestmolecule = copy.deepcopy(molecule)
+            lowestsequence = copy.deepcopy(molecule.sequence)
         i = i + 1
-    molecule = lowestmolecule
+    molecule.sequence = lowestsequence
