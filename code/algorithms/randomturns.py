1e3dcefc22b3e8988b2568aaf33d634a7a10423b
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-11-12 14:19:45 +0100

    readded my spiralfold algorithm(greedy?) and removed import of molecule in randomturns as no new objects are created

--- code/algorithms/randomturns.py
+++ code/algorithms/randomturns.py
@@ -4,9 +4,6 @@ directory = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(directory)
 sys.path.append(os.path.join(parentdir, "objects"))
 
-
-from molecule import Molecule
-
 import random
 import copy
 
