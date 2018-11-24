a2bee5f1cfd9d61e8873460ac1a722b6f8d9e2b7
Maud van Boven <maudrvanboven@gmail.com>
2018-11-24 02:33:03 +0100

    little bit of style

--- code/algorithms/annealing.py
+++ code/algorithms/annealing.py
@@ -12,28 +12,36 @@ from molecule import Molecule
 from randomturns import randomturns
 from greedyfold import spiralfold
 
+
 def aneal(molecule):
     temperature = 200
 
-
     while temperature > 0.1:
+
         print(f"Temp: {temperature}")
         print(f"stability: {molecule.stability()}")
         currentstability = molecule.stability()
         oldmolecule = copy.deepcopy(molecule)
         randomturns(molecule, random.randint(0, 3))
+
         if not molecule.check_vadility():
              molecule = oldmolecule
         elif molecule.stability() < currentstability:
             temperature *= 0.999
         else:
             temperature *= 0.999
-            acceptprobability = math.exp(((currentstability - molecule.stability()) * 80) / temperature)
+            acceptprobability = math.exp(((currentstability -
+                                           molecule.stability()) * 80)
+                                         / temperature)
             print(acceptprobability)
             x = random.uniform(0,1)
+            
             if acceptprobability < x:
                 molecule = oldmolecule
+
     return molecule
+
+
 if __name__ == '__main__':
     molecule = Molecule('HHPHHHPH', 'direct')
     molecule = aneal(molecule)
