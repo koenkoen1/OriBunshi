c8b439f1ecc1d5942c2a2dc8c4e10ffcac0d6004
Maud van Boven <maudrvanboven@gmail.com>
2018-11-29 11:02:24 +0100

    csv outputter data

--- code/algorithms/annealing.py
+++ code/algorithms/annealing.py
@@ -5,6 +5,8 @@ parentdir = os.path.dirname(directory)
 sys.path.append(os.path.join(parentdir, "objects"))
 
 import copy
+import csv
+import datetime
 import math
 import random
 from amino_acid import Amino_Acid
@@ -14,22 +16,66 @@ from greedyfold import spiralfold
 
 BEGINTEMP = 200
 
+
+def filepath():
+    """
+    Outputs a filepath for saving output data.
+    """
+
+    # save output in results directory
+    path = "./results/"
+
+    # increment filename with 1 wrt last file, to not overwrite it
+    i = 0
+    while os.path.exists(f"{path}annealing{i}.csv"):
+        i += 1
+
+    filepath = f"{path}annealing{i}.csv"
+
+    return filepath
+
+
 def tempfunc(k):
     return  (BEGINTEMP / (1 + math.log10(1 + k)))
+
 
 def kfunc(temp):
     return 10 ** (BEGINTEMP/temp - 1) - 1
 
-def anneal(molecule):
+
+def write_csv(outfile, data, molecule):
+    """
+    Output a CSV file with given data.
+    """
+    writer = csv.writer(outfile)
+
+    # write header
+    writer.writerow([datetime.datetime.now(),])
+    writer.writerow([f'sequence = {molecule.sequence}',])
+    writer.writerow([f'start temperature = {BEGINTEMP}',])
+    writer.writerow(['temperature', 'stability'])
+
+    # iterate over and write movies
+    for iteration in data:
+        writer.writerow(iteration)
+
+
+def anneal(molecule, save_data=False):
     spiralfold(molecule, len(molecule.sequence))
     k = 0
     temperature = tempfunc(k)
     reheat = 0
+    data = []
+
     while reheat < 2:
         k += 1
-        print(f"Temp: {temperature}")
+        # print(f"Temp: {temperature}")
         currentstability = molecule.stability()
-        print(f"stability: {currentstability}")
+        # print(f"stability: {currentstability}")
+
+        save_iter = [temperature, currentstability]
+        data.append(save_iter)
+
         oldmolecule = copy.deepcopy(molecule)
         randomturns(molecule, random.randint(0, 3))
 
@@ -39,12 +85,18 @@ def anneal(molecule):
             temperature = tempfunc(k)
         else:
             temperature = tempfunc(k)
-            acceptprobability = math.exp(((currentstability - molecule.stability()) * 175) / temperature)
+            acceptprobability = math.exp(((currentstability -
+                                           molecule.stability()) * 175)
+                                         / temperature)
             x = random.uniform(0,1)
             if acceptprobability < x:
                 molecule = oldmolecule
         if temperature < 37:
             k = kfunc(50)
             reheat += 1
+
+    if save_data:
+        with open(filepath(), 'w', newline='') as output_file:
+            write_csv(output_file, data, molecule)
 
     return molecule
