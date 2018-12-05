83a3bb3988388cbf7d2d08992d539d49d16bc929
lameturkey <koenk1@gmail.com>
2018-12-05 23:30:17 +0100

    proper name and works

---
+++ code/algorithms/hillclimb.py
@@ -0,0 +1,67 @@
+import os
+import sys
+directory = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(directory)
+sys.path.append(os.path.join(parentdir, "objects"))
+
+import copy
+import datetime
+import math
+import random
+from amino_acid import Amino_Acid
+from molecule import Molecule
+from randomturns import randomturns
+from greedyfold import spiralfold
+from write_csv import write_csv
+
+
+
+# function that copies the coordinates from another molecule
+def copylocations(molecule1, molecule2):
+    for index, amino_acid in enumerate(molecule2.acids):
+        molecule1.acids[index].coordinates = amino_acid.coordinates
+
+
+def hillclimb(molecule, save_data=False):
+    loweststability = 1
+    oldmolecule = copy.deepcopy(molecule)
+    k = 0
+    data = []
+    while k < 100:
+        k += 1
+        oldstability = molecule.stability()
+        lowestmolecule = False
+        save_iter = [k, oldstability]
+        data.append(save_iter)
+
+        copylocations(oldmolecule, molecule)
+        randomturns(molecule, random.randint(1, 3))
+
+        while not molecule.check_vadility():
+            copylocations(molecule, oldmolecule)
+            randomturns(molecule, random.randint(1, 3))
+        currentstability = molecule.stability()
+
+        if molecule.stability() <= oldstability:
+            if molecule.stability() < loweststability:
+                lowestmolecule = copy.deepcopy(molecule)
+                loweststability = currentstability
+        else:
+            copylocations(molecule, oldmolecule)
+
+    # write data to csv file if save option was chosen
+    if save_data:
+        header = ['temperature', 'stability',
+                  datetime.datetime.now(),
+                  f'sequence = {molecule.sequence}']
+
+        write_csv("annealing", header, data)
+
+    if lowestmolecule:
+        return lowestmolecule
+    else:
+        return molecule
+
+if __name__ == '__main__':
+    molecule = shortanneal(Molecule("HHPHHPHP", "direct"))
+    molecule.draw()
---
+++ code/algorithms/populationbased.py
@@ -0,0 +1,56 @@
+import os
+import sys
+directory = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(directory)
+sys.path.append(os.path.join(parentdir, "objects"))
+
+import copy
+import datetime
+import math
+import random
+from amino_acid import Amino_Acid
+from molecule import Molecule
+from randomturns import randomturns
+from greedyfold import spiralfold
+from write_csv import write_csv
+from hillclimb import hillclimb
+from operator import itemgetter
+import datetime
+
+def geneteic(sequence):
+    print(datetime.datetime.now())
+    data = []
+    pop = []
+    maxiterations = 10
+    for i in range(10):
+        molecule = Molecule(sequence, "random")
+        pop.append([molecule, molecule.stability()])
+    iterations = 0
+    while iterations < maxiterations:
+        print(iterations)
+        newpop = []
+        list = [list[0] for list in pop]
+        for molecule in list:
+            molecule1 = hillclimb(molecule)
+            molecule2 = hillclimb(molecule)
+            newpop.append([molecule1, molecule1.stability()])
+            newpop.append([molecule2, molecule2.stability()])
+        pop = newpop
+        pop = sorted(pop, key=itemgetter(1))
+        pop = pop[:int(len(pop)/2)]
+        datalist = [item[1] for item in pop]
+        data.append([iterations, sum(datalist) / len(datalist)])
+        iterations += 1
+    print(datetime.datetime.now())
+    pop[0][0].draw()
+    header = ['temperature', 'stability',
+              datetime.datetime.now(),
+              f'sequence = {molecule.sequence}']
+
+    write_csv("annealing", header, data)
+
+
+
+
+if __name__ == '__main__':
+    geneteic("PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP")
