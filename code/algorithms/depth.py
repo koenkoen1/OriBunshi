632f59409dcb447bcf993a6b7fab80aa8e50b072
koenkoen1 <43954486+koenkoen1@users.noreply.github.com>
2018-12-17 17:19:40 +0100

    depth and randomsample checked

--- code/algorithms/depth.py
+++ code/algorithms/depth.py
@@ -2,8 +2,8 @@ import copy
 from amino_acid import Amino_Acid
 from molecule import Molecule
 
+# global list of unique valid molecule objects
 stack = []
-
 
 def depth(sequence):
     """
@@ -13,7 +13,7 @@ def depth(sequence):
     Pruning: if a child molecule is found which isn't valid the child is removed
     prints the whole solution space when its done
     """
-    # produces a hardcoded stability list
+    # produce list with max amount of possible stabilities, to count solutions
     solutions = [0 for i in range(len(sequence))]
 
     # produces the first amino acid of the molecule as the molecule
@@ -22,13 +22,13 @@ def depth(sequence):
     # append this molecule to the stack
     stack.append(molecule)
 
-    # define the lowest stability found (placeholder = 0) + placeholder molecule
+    # define the lowest stability found (placeholder = 1) + placeholder molecule
     lowest = 1
     lowestmolecule = Molecule("H", "direct")
     while stack != []:
         current = stack.pop()
 
-        # if the length of the sequence equals the input sequence dont make children
+        # if the length of the sequence equals the input sequence, dont make children
         if len(current.sequence) == len(sequence):
             solutions[int(-current.stability())] += 1
             if lowest > current.stability():
@@ -43,7 +43,7 @@ def depth(sequence):
         print(f"stabilty: {i}: {stability}")
         i -= 1
 
-    # draws the first solution molecule found with the lowest stability
+    # draws the first molecule found with the lowest stability
     lowestmolecule.draw()
 
     # returns this molecule for further usage
@@ -56,14 +56,14 @@ def children(molecule, sequence):
     possible location and appends this to the stack
     """
 
-    # every direction
+    # iterate over every possible direction
     for direction in range(4):
         x, y  = molecule.acids[len(molecule.acids) - 1].coordinates
         if direction == 0:
             y += 1
         elif direction == 1:
             x += 1
-        # don't go to y - 1 from straight molecule, to elemininate mirror images
+        # avoid y - 1 from straight molecule, to eliminate mirror images
         elif direction == 2 and not straight(molecule):
             y -= 1
         elif direction == 3:
@@ -72,7 +72,7 @@ def children(molecule, sequence):
         # make a new amino acid
         acid = Amino_Acid(sequence[len(molecule.acids)], (x, y))
 
-        # if adding the acid was succesfull
+        # if resulting molecule is valid, add it to the stack
         if molecule.add_acids([acid]):
             stack.append(copy.deepcopy(molecule))
             molecule.remove_acids([acid])
--- code/algorithms/randomsample.py
+++ code/algorithms/randomsample.py
@@ -6,7 +6,7 @@ from write_csv import write_csv
 
 def randomsample(input_molecule, iterations, save_data=False):
     """
-    Creates Molecule objects based on inputted molecule with a random
+    Creates multiple Molecule objects based on inputted molecule with a random
     configuration and returns the configuration with the best score.
     It requires a Molecule object, the amount of configurations that should be
     created and optionally a boolean to indicate whether the resulting data
@@ -16,14 +16,17 @@ def randomsample(input_molecule, iterations, save_data=False):
     # produce list with max amount of possible stabilities, to count solutions
     solutions = [[-i, 0] for i in range(len(input_molecule.sequence))]
 
-    best_solution = copy.deepcopy(input_molecule)
+    # placeholder molecule for best solution
+    best_solution = input_molecule
 
+    # create random molecules
     for i in range (iterations):
         molecule = Molecule(input_molecule.sequence, 'random')
         solutions[-molecule.stability()][1] += 1
 
+        # save molecule with best stability
         if molecule.stability() < best_solution.stability():
-            best_solution = copy.deepcopy(molecule)
+            best_solution = molecule
 
     # write data to csv file if save option was chosen
     if save_data:
